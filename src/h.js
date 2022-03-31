bb61baac7237ee8bf3275014fe7082eff964b093
F_picacho <2314008827@qq.com>
2022-03-31 19:39:19 +0800

    [add]h() vnode()

---
+++ src/h.js
@@ -0,0 +1,32 @@
+import vnode from "./vnode";
+
+export default function (sel, data, c) {
+    // 相较于原版省去了重载的过程以达到精炼逻辑的目的
+    if (arguments.length !== 3) {
+        throw new Error('sry 乞丐版不支持重载，必须为三个参数！')
+    }
+    /*
+    重载参数c 判断以下三种情况做出不同的反应
+    h('p',{},'嘿嘿～')
+    h('p',{},[h('p',{},'嘿嘿～'),h('p',{},'嘿嘿～')])
+    h('p',{},h('p',{},'嘿嘿～'))
+     */
+    if (typeof c === "string" || typeof c === "number") {
+        return vnode(sel, data, undefined, c, undefined)
+    } else if (Array.isArray(c)) {
+        let children = []
+        c.forEach((item) => {
+            // 注意h函数被调用时会直接返回对象 所以这里直接判断数组项是否为对象且是否存在sel属性即可完成判断
+            if (!(typeof item === "object" && item.hasOwnProperty('sel'))) {
+                throw new Error('传入的数组参数中其中的某一项不为h函数！')
+            }
+            children.push(item)
+        })
+        return vnode(sel, data, children, undefined, undefined)
+    } else if (typeof c === "object" && c.hasOwnProperty('sel')) {
+        let children = [c]
+        return vnode(sel, data, children, undefined, undefined)
+    } else {
+        throw new Error('传入的参数3错误！')
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,57 +1,7 @@
-import {
-    init,
-    classModule,
-    propsModule,
-    styleModule,
-    eventListenersModule,
-    h,
-} from "snabbdom";
+import h from './h'
 
-const patch = init([
-    // 通过传入模块初始化 patch 函数
-    classModule, // 开启 classes 功能
-    propsModule, // 支持传入 props
-    styleModule, // 支持内联样式同时支持动画
-    eventListenersModule, // 添加事件监听
-]);
-
-const container = document.getElementById("container");
-
-const vnode = h(
-    "div#container.two.classes",
-    { on: { click: ()=>{} } },
-    [
-        h(
-            "span",
-            { style: { color: "bold" } },
-            "This is bold"
-        ),
-        " and this is just normal text",
-        h(
-            "a",
-            { props: { href: "/foo" } },
-            "I'll take you places!"
-        ),
-]);
-// 传入一个空的元素节点 - 将产生副作用（修改该节点）
-patch(container, vnode);
-
-const newVnode = h(
-    "div#container.two.classes",
-    { on: { click: ()=>{} } },
-    [
-        h(
-            "span",
-            { style: { fontWeight: "normal", fontStyle: "italic" } },
-            "This is now italic type"
-        ),
-        " and this is still just normal text",
-        h(
-            "a",
-            { props: { href: "/bar" } },
-            "I'll take you places!"
-        ),
-    ]
-);
-// 再次调用 `patch`
-patch(vnode, newVnode); // 将旧节点更新为新节点
+const vnode = h('div', {}, [
+    h('p', {}, '哈哈'),
+    h('p', {}, h("p", {}, "呜呜")),
+])
+console.log(vnode)
